5a7a5b62af6c5334cbab6f324f23a9c1c9c30972
mhsohamzeh <43642884+mhsohamzeh@users.noreply.github.com>
2018-11-22 10:29:49 -0500

    Add files via upload

--- p5/sound/sketch.js
+++ p5/sound/sketch.js
@@ -1,8 +1,8 @@
 var mysound
 var img;
 function preload(){
- img = loadImage("kisscc0-tom-cat-jerry-mouse-tom-and-jerry-cartoon-characte-tom-tom-and-jerry-5b4ec6f75e9de6.8274577815318893993876")
- mysound = loadSound ("AHHH!1")
+ img = loadImage("kisscc0-tom-cat-jerry-mouse-tom-and-jerry-cartoon-characte-tom-tom-and-jerry-5b4ec6f75e9de6.8274577815318893993876.jpg")
+ mysound = loadSound ("AHHH!1.wav")
 }
 
 function setup() {
